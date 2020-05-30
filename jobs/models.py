405d8409733ce09fa82e11dfe598df7b206a3025
uditsg <uditsengupta54@gmail.com>
2020-05-30 17:08:47 +0530

    Changing settings.py to connect to Postgres

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Job(models.Model):
+    # Image Property
+    image = models.ImageField(upload_to='images/')
+    # Summary Property
+    summary = models.CharField(max_length=200)
