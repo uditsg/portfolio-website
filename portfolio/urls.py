28070f39dc9463698ef1181f38124bd5628fad0e
uditsg <uditsengupta54@gmail.com>
2020-05-30 23:31:19 +0530

    Adding code to link the jobs listed  on the home page to the job details page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home',jobs.views.home, name='home')
+    path('',jobs.views.home, name='home'),
+    path('jobs/<int:job_id>', jobs.views.detail, name='detail'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
